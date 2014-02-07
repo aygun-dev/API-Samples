2795218905f219272bc2860ddfadbfebb9dc3db9
Mohammad Rafsan <mohammad@lagoa.com>
2014-02-07 18:15:30 -0500

    Remove callbacks for addNewMaterial and addNewLight.
    They are no longer necessary with new changes.

--- src/scene.js
+++ src/scene.js
@@ -117,10 +117,8 @@ lapi.Scene.prototype = {
   /**
    * Add a new material to scene.
    * @param {string} in_materialType  The type of material the user wants to add : 'Glossy Diffuse','Architectural Glass' etc.
-   * @param {function} in_cb  optional callback that expects a material SceneObject as an argument. The object is the one we just added
-   * to the scene through addNewMaterial().
    */
-  addNewMaterial : function(in_materialType,in_cb){
+  addNewMaterial : function(in_materialType){
     var self = this;
     lapi._embedRPC("var mat = ACTIVEAPP.AddEngineMaterial({minortype : '"
     + in_materialType + "'});");
@@ -129,10 +127,8 @@ lapi.Scene.prototype = {
   /**
    * Add a new light to scene.
    * @param {string} in_lightType  The type of light the user wants to add : 'DomeLight','SunSkyLight' etc.
-   * @param {function} in_cb  optional callback that expects a light SceneObject as an argument. The object is the 
-   * one we just added to the scene through addNewLight().
    */
-  addNewLight : function(in_lightType,in_cb){
+  addNewLight : function(in_lightType){
     var self = this;
     lapi._embedRPC("var light = ACTIVEAPP.AddLight({minortype : '"
     + in_lightType + "'});");
