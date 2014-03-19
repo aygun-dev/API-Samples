91d08c6264e7835330e30a95b89f7c6e6e83446e
jackquack <jack.quack@gmail.com>
2014-03-19 16:04:54 -0400

    Use a local environment variable "LAGOA_ACCESS_TOKEN" as the default access token. Add support for image options which are passed on the command line.

--- samples/platform/cli_upload/nodejs/README.md
+++ samples/platform/cli_upload/nodejs/README.md
@@ -9,9 +9,16 @@ This sample app can be used to upload an asset to Lagoa via the command line.
 
     ACCESS_TOKEN = 'thisismytoken';
 
+If available, we'll use an environment variable called LAGOA_ACCESS_TOKEN.
+
 ## How to use
 
 node uploadFile.js --name=<Friendly File Name.ext> --path=<path to file>
+
+There are some options which can be sent:
+
+2D images
+  --image-gamma=<number>
 
 Note: The friendly file name needs to have a [supported file extension](http://support.lagoa.com/document/file-formats-2/)
 
--- samples/platform/cli_upload/nodejs/uploadFile.js
+++ samples/platform/cli_upload/nodejs/uploadFile.js
@@ -15,8 +15,7 @@ var request = require('request')
 
 var host = 'https://api.lagoa.com',
   api_path = '/v1',
-  ACCESS_TOKEN = '';
-
+  ACCESS_TOKEN = process.env.LAGOA_ACCESS_TOKEN;
 
 var filename = args.name;
 var filepath = args.path;
@@ -31,7 +30,20 @@ if ( !ACCESS_TOKEN ) {
   process.exit(0);
 }
 
+/***
+* Pull out the user user options
+*/
+
+var options = {};
+for (var itr in args) {
+  if (itr.indexOf('image') != -1){
+    options.image = options.image || {};
+    options.image[itr.split('-')[1]] = args[itr];
+  }
+}
+
 console.log(filename + ', ' + filepath);
+console.log(options);
 
 async.waterfall([
   function (cb) { cb(null, filename, filepath);}
@@ -48,7 +60,7 @@ function createNewSession(asset_name, asset_path, cb) {
     url: util.format("%s%s%s.json", host, api_path, "/upload_sessions"),
     json: {
       access_token: ACCESS_TOKEN,
-      files: [{ name: asset_name }]
+      files: [{ name: asset_name, options: options}]
     }
   }, function(error, response, body) {
     cb(error, body.files[0].url, asset_name, asset_path, body.session_token);
